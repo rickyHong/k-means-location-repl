85549d6ef6989fd8bca81c73db2b5a545eecc176
rickyHong <jh74.hong@samsung.com>
2020-02-10 17:30:50 +0900

    Update point.py
    add index

--- point.py
+++ point.py
@@ -1,4 +1,5 @@
 class Point:
-    def __init__(self, latit_, longit_):
+    def __init__(self, latit_, longit_, index_):
         self.latit = latit_
         self.longit = longit_
+        self.index = index_
